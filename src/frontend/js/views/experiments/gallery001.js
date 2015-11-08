a64c6b9531281efa03bb94305adf2628ad28b0e9
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-11-07 21:20:22 -0500

    gallery experiment

---
+++ src/frontend/js/views/experiments/gallery001.js
@@ -0,0 +1,8 @@
+/** extract the categories */
+// var categories = [];
+// $.each(configs.gallery, function(i,e){
+//     categories.push(e.category);
+// });
+// categories = _.uniq(categories);
+
+console.log(configs);
