3297a31b5353002b51b76c938a01624ebffdaba4
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-12-06 22:08:43 -0500

    change orientation on thumbnail change

--- src/frontend/js/library/views/CharacterCardView.js
+++ src/frontend/js/library/views/CharacterCardView.js
@@ -5,8 +5,12 @@ var CharacterCardView = Backbone.View.extend({
         this.model = options.model;
         $(this.el).find('thumbnail').click(function(e){
             $(that.el).find('thumbnail').removeClass('active');
+            $(that.el).find('preview img').removeClass('portrait').removeClass('landscape');
             $(e.currentTarget).addClass('active');
             $(that.el).find('preview img').attr('src', $(e.currentTarget).find('img').attr('src'));
+            $(that.el).find('preview img').addClass(
+              that.model.get('images')[$(e.currentTarget).find('img').attr('data-image-index')].orientation
+            );
         });
         this.render();
     }
