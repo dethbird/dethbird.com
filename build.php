fe573f836fb4c09ed3b202ac9849b81aed04f319
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-01-17 08:55:21 -0500

    build fix, added npm install

--- build.php
+++ build.php
@@ -31,6 +31,12 @@ if($options['php']!="") {
   shell_exec("rm -rf composer.phar");
 }
 
+// node
+if($options['node']!="") {
+    shell_exec("rm -rf node_modules");
+    shell_exec("npm install");
+}
+
 // javascript
 if($options['js']!="") {
   echo "js:".$options['js']."\n";
