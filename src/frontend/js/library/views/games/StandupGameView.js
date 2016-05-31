cb75cc542117eb4b28ea2bb0e758217cac87e11d
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-05-31 23:01:30 +0000

    added view class with Craftyjs init for StandupGame

---
+++ src/frontend/js/library/views/games/StandupGameView.js
@@ -0,0 +1,12 @@
+var Crafty = require('craftyjs');
+var StandupGameView = Backbone.View.extend({
+  crafty: null,
+    initialize: function(options) {
+        var that = this;
+
+        that.crafty = Crafty.init(500,350, $(that.el).attr('id'));
+        that.crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
+    }
+});
+
+module.exports = StandupGameView;
---
+++ src/frontend/js/pages/experiments/standupgame.js
@@ -0,0 +1,7 @@
+var StandupGameView = require('../../library/views/games/StandupGameView');
+
+var standupGameView = new StandupGameView({
+    el: '#game'
+});
+
+console.log(standupGameView);
