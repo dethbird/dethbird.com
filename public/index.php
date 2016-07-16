4f2f15a80e24168d046311f1fc2da36842dc292a
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-07-16 09:33:22 -0400

    pulling project from workstation

--- public/index.php
+++ public/index.php
@@ -17,16 +17,14 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 
 require '../vendor/autoload.php';
-require_once APPLICATION_PATH . 'src/library/ExternalData/InstagramData.php';
-require_once APPLICATION_PATH . 'src/library/ExternalData/YoutubeData.php';
-require_once APPLICATION_PATH . 'src/library/ExternalData/WordpressData.php';
 require_once APPLICATION_PATH . 'src/library/View/Extension/TemplateHelpers.php';
 
 use Aptoma\Twig\Extension\MarkdownExtension;
 use Aptoma\Twig\Extension\MarkdownEngine;
 use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Client;
 
-// Load configs and add to the app container
+# Load configs and add to the app container
 $configs = Yaml::parse(file_get_contents("../configs/configs.yml"));
 $app = new \Slim\Slim(
     array(
@@ -45,10 +43,9 @@ $view->parserExtensions = array(
     new TemplateHelpers(),
     new MarkdownExtension($markdownEngine)
 );
-// $view->addExtension(new MarkdownExtension($markdownEngine));
 $app->container->set('configs', $configs);
 
-// Route authentication
+# security context auth
 $authenticate = function ($app) {
     // @todo does logged in user have access to this project?
     return function () use ($app) {
@@ -79,12 +76,15 @@ $authenticate = function ($app) {
 };
 
 
+# 404
 $app->notFound(function () use ($app) {
     $app->render(
         'pages/404.html.twig'
     );
 });
 
+
+# login
 $app->get("/login", function () use ($app) {
   // die(var_dump($_SESSION));
   $app->render(
@@ -92,6 +92,8 @@ $app->get("/login", function () use ($app) {
       array()
   );
 });
+
+# login api
 $app->post("/login", function () use ($app) {
 
     $app->response->headers->set('Content-Type', 'application/json');
@@ -127,106 +129,31 @@ $app->get("/logout", function () use ($app) {
 
 $app->get("/", function () use ($app) {
     $app->redirect("http://rishisatsangi.com");
-    $configs = $app->container->get('configs');
-
-    $_gallery = array();
-    foreach($configs['portfolio']['gallery'] as $item) {
-        $item['id'] = md5($item['thumbnail']);
-        $_gallery[] = $item;
-    }
-    $configs['portfolio']['gallery'] = $_gallery;
-
-    $instagramData = new InstagramData($configs['instagram']['client_id']);
-    //$wordpressData = new WordpressData($configs['wordpress']['url']);
-
-    $templateVars = array(
-        "configs" => $configs,
-        "instagramData" => $instagramData->getRecentMedia($configs['instagram']['user_id'], 60, array(
-            "art",
-            "drawing",
-            "sketchbook"
-        )),
-        //"wordpressData" => $wordpressData->getPosts($configs['wordpress']['posts']),
-        "tweetIds" => $configs['twitter']['tweets']
-    );
-    $app->render(
-        'pages/index.html.twig',
-        $templateVars,
-        200
-    );
-});
-
-$app->get("/comics/:name", function ($name) use ($app) {
-    $configs = $app->container->get('configs');
-    if(!array_key_exists($name, $configs["comics"])){
-        $app->notFound();
-    } else {
-        $app->render(
-            'pages/comics.html.twig',
-            array("comic" => $configs["comics"][$name]),
-            200
-        );
-    }
-});
-
-$app->get("/resume", function () use ($app) {
-    $resume = Yaml::parse(file_get_contents("../configs/resume.yml"));
-
-    if ($app->request->isAjax()) {
-        $app->response->headers->set('Content-Type', 'application/json');
-        $app->response->setBody(json_encode($resume));
-    } else {
-        $app->render(
-            'pages/resume.html.twig',
-            $resume,
-            200
-        );
-    }
-});
-
-/**
- * Experimental pages for proof of concepting
- */
-$app->get("/experiments/:name", $authenticate($app), function ($name) use ($app) {
-
-    $configs = $app->container->get('configs');
-    $experimentConfigs = Yaml::parse(file_get_contents("../configs/experiments.yml"));
-
-    $templateVars = array(
-        "configs" => $configs,
-        "experimentConfigs" => isset($experimentConfigs[$name]) ? $experimentConfigs[$name] : array()
-    );
-    if($name=="gallery001") {
-        $instagramData = new InstagramData($configs['instagram']['client_id']);
-        $templateVars['instagramData'] = $instagramData->getRecentMedia($configs['instagram']['user_id'], 60, array(
-            "art",
-            "drawing",
-            "sketchbook"
-        ));
-    }
-
-    if($name=="fountain002") {
-        $templateVars['fountain'] = file_get_contents($experimentConfigs[$name]['fountainfile']);
-    }
-
-    $app->render(
-        'pages/experiments/'. $name .'.html.twig',
-        $templateVars,
-        200
-    );
 });
 
 
-/** public / private project wikis **/
+
+# projects
 $app->get("/projects/:name", $authenticate($app), function ($name) use ($app) {
 
     $configs = $app->container->get('configs');
-    $projectConfigs = Yaml::parse(file_get_contents("../configs/projects/".$name."/index.yml"));
+    $projectConfigs = Yaml::parse(file_get_contents("../configs/projects.yml"));
+    $projectId = $projectConfigs[$name];
+
+    $httpClient = new Client([
+        "base_uri" => $configs['workstation']['api_url']]);
+    $response = $httpClient->request("GET", "/api/project/" . $projectId, [
+        "headers" => [
+            "Auth-Token" => $configs['workstation']['auth_token']
+        ]
+    ]);
+    $body = $response->getBody();
+    // print_r(json_decode($body)); exit();
     $templateVars = array(
         "configs" => $configs,
         "securityContext" => json_decode($app->getCookie('securityContext')),
         "currentProject" => $name,
-        "projectConfigs" => $projectConfigs
+        "project" => json_decode($body)
     );
 
     $app->render(
@@ -237,81 +164,6 @@ $app->get("/projects/:name", $authenticate($app), function ($name) use ($app) {
 
 });
 
-
-/** publically acceessible notes */
-$app->get("/notes", function () use ($app) {
-
-    // get directory listing for notes
-    $directory = new \RecursiveDirectoryIterator(APPLICATION_PATH . "/configs/notes");
-    $objects = new RecursiveIteratorIterator($directory);
-    $files = array();
-
-    foreach($objects as $filepath => $fileinfo){
-        $file = new stdClass();
-        $file->name = $fileinfo->getFilename();
-        $file->name = $fileinfo->getFilename();
-        $file->mtime = $fileinfo->getMTime();
-        $files[] = $file;
-    }
-
-    $configs = $app->container->get('configs');
-    $templateVars = array(
-        "configs" => $configs,
-        "securityContext" => json_decode($app->getCookie('securityContext')),
-        "files" => $files
-    );
-
-    $app->render(
-        'pages/notes/index.html.twig',
-        $templateVars,
-        200
-    );
-});
-
-$app->get("/note/:name", function ($name) use ($app) {
-    echo json_encode($_SERVER); die();
-    $configs = $app->container->get('configs');
-    $noteMarkdown = file_get_contents("../configs/notes/".$name.".md");
-    $templateVars = array(
-        "configs" => $configs,
-        "securityContext" => json_decode($app->getCookie('securityContext')),
-        "currentNote" => $name,
-        "noteMarkdown" => $noteMarkdown
-    );
-
-    $app->render(
-        'pages/notes/note.html.twig',
-        $templateVars,
-        200
-    );
-});
-//
-// $app->post("/note/:name", $authenticate($app), function ($name) use ($app) {
-//
-//     $bytes = file_put_contents("../configs/notes/".$name.".md", $app->request->params('content'));
-//     $data = new stdClass();
-//     $data->bytesWritten = $bytes;
-//
-//     $app->response->headers->set('Content-Type', 'application/json');
-//     $app->response->setBody(json_encode($data));
-// });
-//
-
-$app->get("/posts/:type", $authenticate($app), function ($type) use ($app) {
-
-    $app->response->headers->set('Content-Type', 'application/json');
-
-    if($type=="instagram") {
-        $instagramData = new InstagramData();
-        $data = $instagramData->getMedia($app->request->params('ids'));
-    } else if($type=="wordpress") {
-        $wordpressData = new WordpressData();
-        $data = $wordpressData->getPosts($app->request->params('ids'));
-    }
-
-    $app->response->setBody(json_encode($data));
-
-});
 
 
 $app->run();
