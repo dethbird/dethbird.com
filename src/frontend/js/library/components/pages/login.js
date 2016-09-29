2b2e2785662670d759d1fe5fb318402020276bf1
Rishi Satsangi <rishi.satsangi@gmail.com>
2016-09-29 07:41:20 -0400

    react login form

---
+++ src/frontend/js/library/components/pages/login.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { browserHistory } from 'react-router'
+
+import { Alert } from "../ui/alert"
+import { Card } from "../ui/card"
+import { CardBlock } from "../ui/card-block"
+import { SectionHeader } from "../ui/section-header"
+import { Spinner } from "../ui/spinner"
+
+
+const Login = React.createClass({
+    getInitialState() {
+        return ({
+            model: {},
+            formState: null,
+            formMessage: null,
+            changedFields: {}
+        });
+    },
+    handleFieldChange(event) {
+        let model = this.state.model;
+        let changedFields = this.state.changedFields;
+
+        model[event.target.id] = event.target.value
+        changedFields[event.target.id] = event.target.value
+
+        this.setState({
+            model: model,
+            changedFields: changedFields
+        })
+    },
+    handleClickSubmit(event) {
+        event.preventDefault()
+        var that = this
+        $.ajax({
+            data: that.state.changedFields,
+            dataType: 'json',
+            cache: false,
+            method: 'POST',
+            url: '/api/login',
+            beforeSend: function() {
+                this.setState({
+                    formState: 'info',
+                    formMessage: 'Working.',
+                })
+            }.bind(this),
+            success: function(data) {
+                this.setState({
+                    formState: 'success',
+                    formMessage: 'Success.',
+                    model: data
+                })
+                document.location = data.redirectTo
+            }.bind(this),
+            error: function(xhr, status, err) {
+                this.setState({
+                    formState: 'danger',
+                    formMessage: 'Error: ' + xhr.responseText
+                })
+            }.bind(this)
+        });
+    },
+    render() {
+        let that = this
+        if (this.state){
+            return (
+                <div>
+                    <Alert
+                        status={ this.state.formState }
+                        message={ this.state.formMessage }
+                    />
+                    <form>
+                        <h1>Sign In, Honcho</h1>
+                        <SectionHeader>username:</SectionHeader>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
+                                placeholder="Username"
+                                value={ this.state.model.username }
+                                onChange= { this.handleFieldChange }
+                            />
+                        </div>
+
+                        <SectionHeader>password:</SectionHeader>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                rows="3"
+                                value={ this.state.model.password || '' }
+                                onChange= { this.handleFieldChange }
+                            />
+                        </div>
+
+                        <div className="form-group text-align-center">
+                            <button
+                                className="btn btn-success"
+                                onClick={ that.handleClickSubmit }
+                                disabled={ !that.state.changedFields }
+                            >Login</button>
+                        </div>
+
+
+                    </form>
+                </div>
+            );
+        }
+        return (
+            <Spinner />
+        )
+    }
+})
+
+module.exports.Login = Login
--- src/frontend/js/pages/login.js
+++ src/frontend/js/pages/login.js
@@ -1,5 +1,25 @@
-var LoginFormView = require('../library/views/forms/LoginFormView');
+import React from 'react'
+import { render } from 'react-dom'
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
-var loginFormView = new LoginFormView({
-    el: '#login_form'
-});
+import { App } from '../library/components/app'
+import { Login } from '../library/components/pages/login'
+
+
+const NoMatch = React.createClass({
+  render() {
+    return (
+      <div>Whachhu talkin about</div>
+    )
+  }
+})
+
+render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Login}/>
+            <Route path="login" component={Login}/>
+            <Route path="*" component={Login}/>
+        </Route>
+    </Router>
+), document.getElementById('mount'))
