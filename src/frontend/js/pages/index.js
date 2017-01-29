d66e352a434a1f215f804fa4fb67fff25f87879f
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-01-29 12:08:42 -0500

    fix

---
+++ src/frontend/js/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+
+import { App } from '../components/app'
+import Index from '../components/pages/index'
+import store from '../store/store';
+
+const NoMatch = React.createClass({
+  render() {
+    return (
+      <div>Whachhu talkin about</div>
+    )
+  }
+})
+
+if (lastRequestUri) {
+    browserHistory.push(lastRequestUri);
+}
+
+render((
+    <Provider store={ store }>
+        <Router history={browserHistory}>
+            <Route path="/" component={ App}>
+                <IndexRoute component={ Index } />
+                <Route path="*" component={ Index } />
+            </Route>
+        </Router>
+    </Provider>
+), document.getElementById('mount'))
