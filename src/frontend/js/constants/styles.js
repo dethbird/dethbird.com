7cef29ecca30b58c3e82d9cfa067e629f315b3e3
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-03-22 08:57:48 -0400

    index page test

--- src/frontend/js/constants/styles.js
+++
@@ -1,7 +0,0 @@
-export const buttonStyle = {
-    margin: '10px'
-};
-
-export const cardHeaderStyle = {
-    fontSize: '18px'
-}
---
+++ src/frontend/js/pages/__tests__/index.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import {
+    Segment,
+    Sidebar,
+} from 'semantic-ui-react';
+
+import Index from 'components/pages/index';
+
+import LoginForm from 'components/ui/form/login-form';
+import Footer from 'components/ui/footer';
+import Masthead from 'components/ui/masthead';
+
+describe('Index page', () => {
+    test('render()', () => {
+        const props = {
+            route: {
+                props: {
+                    securityContext: {}
+                }
+            }
+        };
+        const component = mount(
+            <Index { ... props } />
+        );
+        expect(component.find(Masthead).length).toEqual(1);
+        expect(component.find(Masthead).props().onClickLogin !== undefined);
+        expect(component.find(LoginForm).length).toEqual(1);
+        expect(component.find(LoginForm).props().onClickCancel !== undefined);
+    });
+});
