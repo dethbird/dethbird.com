2a4284bc826f02c51abeb8d1e1a44191a47b58e5
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-03-29 19:43:37 -0400

    fountain parser in progress

--- src/frontend/js/utility/__tests__/fountain-parser.spec.js
+++ src/frontend/js/utility/__tests__/fountain-parser.spec.js
@@ -1,12 +1,54 @@
-import fountainParser from 'utility/fountain-parser';
+import fs from 'fs';
+import {
+    compileTokens,
+    lexizeScript,
+    parseFountainScript,
+    tokenizeLines
+} from 'utility/fountain-parser';
 
-describe.only('fountainParser', () => {
-    test('parse', () => {
-        const parsed = fountainParser.parse("INT. A PANTS STORT\n\nGOOBER\nI like pie.\n\nBUTTHOLEFACE\nMe toops");
-        console.log(parsed);
-        // expect(fields.username !== undefined);
-        // expect(fields.username == null);
-        // expect(fields.password !== undefined);
-        // expect(fields.username == null);
+const dataDir = process.cwd() + '/src/frontend/js/utility/__tests__/data';
+
+// example data
+const exampleDialogue = fs.readFileSync(dataDir + '/dialogue.fountain', 'utf8');
+const exampleNotes = fs.readFileSync(dataDir + '/notes.fountain', 'utf8');
+const examplePageBreaks = fs.readFileSync(dataDir + '/page_break.fountain', 'utf8');
+const exampleSceneHeadings = fs.readFileSync(dataDir + '/scene_headings.fountain', 'utf8');
+const exampleSections = fs.readFileSync(dataDir + '/sections.fountain', 'utf8');
+const exampleTitlePage = fs.readFileSync(dataDir + '/title_page.fountain', 'utf8');
+const exampleTransitions = fs.readFileSync(dataDir + '/transitions.fountain', 'utf8');
+
+describe('fountainParser.tokenizeLines()', () => {
+    test.only('tokenize dialogue', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleDialogue));
+        console.log(tokens);
+    });
+    test('tokenize page_breaks', () => {
+        const tokens = tokenizeLines(lexizeScript(examplePageBreaks));
+        console.log(tokens);
+    });
+    test('tokenize notes', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleNotes));
+        console.log(tokens);
+    });
+    test('tokenize scene_headings', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleSceneHeadings));
+        console.log(tokens);
+    });
+    test('tokenize sections', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleSections));
+        console.log(tokens);
+    });
+    test('tokenize title_page', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleTitlePage));
+        console.log(tokens);
+    });
+    test('tokenize transitions', () => {
+        const tokens = tokenizeLines(lexizeScript(exampleTransitions));
+        console.log(tokens);
+    });
+
+    test('compile dialogue', () => {
+        const compiled = compileTokens(tokenizeLines(lexizeScript(exampleDialogue)));
+        console.log(compiled);
     });
 });
--- src/frontend/js/utility/fountain-parser.js
+++ src/frontend/js/utility/fountain-parser.js
@@ -1,26 +1,36 @@
-const regex = {
-    title_page: /^((?:title|credit|author[s]?|source|notes|draft date|date|contact|copyright)\:)/gim,
+export const REGEX = {
 
-    scene_heading: /^((?:\*{0,3}_?)?(?:(?:int|ext|est|i\/e)[. ]).+)|^(?:\.(?!\.+))(.+)/i,
-    scene_number: /( *#(.+)# *)/,
+    LEXER: {
+        BONEYARD: /(^\/\*|^\*\/)$/g,
+        SPLITTER: /\n{2,}/g,
+        CLEANER: /^\n+|\n+$/,
+        STANDARDIZER: /\r\n|\r/g,
+        WHITESPACER: /^\t+|^ {3,}/gm
+    },
 
-    transition: /^((?:FADE (?:TO BLACK|OUT)|CUT TO BLACK)\.|.+ TO\:)|^(?:> *)(.+)/,
+    TITLE_PAGE: /^((?:title|credit|author[s]?|source|notes|draft date|date|contact|copyright)\:)/gim,
 
-    dialogue: /^([A-Z*_]+[0-9A-Z (._\-')]*)(\^?)?(?:\n(?!\n+))([\s\S]+)/,
-    parenthetical: /^(\(.+\))$/,
+    SCENE_HEADING: /^((?:\*{0,3}_?)?(?:(?:int|ext|est|i\/e)[. ]).+)|^(?:\.(?!\.+))(.+)/i,
+    SCENE_NUMBER: /( *#(.+)# *)/,
+
+    TRANSITION: /^((?:FADE (?:TO BLACK|OUT)|CUT TO BLACK)\.|.+ TO\:)|^(?:> *)(.+)/,
+
+    DIALOGUE: /^([A-Z*_]+[0-9A-Z (._\-')]*)(\^?)?(?:\n(?!\n+))([\s\S]+)/,
+    DIALOGUE_POWER_USER: /^(?:[@])([a-zA-Z*_]+[0-9A-Z (._\-')]*)(\^?)?(?:\n)([\s\S]+)/,
+    PARENTHETICAL_DIALOGUE: /(\(.+\))(?:\n+)/,
+
+    SECTION: /^(#+)(?: *)(.*)/,
+    SYNOPSIS: /^(?:\=(?!\=+) *)(.*)/,
 
     action: /^(.+)/g,
-    centered: /^(?:> *)(.+)(?: *<)(\n.+)*/g,
+    CENTERED: /^(?:> *)(.+)(?: *<)(\n.+)*/g,
 
-    section: /^(#+)(?: *)(.*)/,
-    synopsis: /^(?:\=(?!\=+) *)(.*)/,
-
-    note: /^(?:\[{2}(?!\[+))(.+)(?:\]{2}(?!\[+))$/,
+    NOTE: /^(?:\[{2}(?!\[+))(.+)(?:\]{2}(?!\[+))$/,
     note_inline: /(?:\[{2}(?!\[+))([\s\S]+?)(?:\]{2}(?!\[+))/g,
-    boneyard: /(^\/\*|^\*\/)$/g,
 
-    page_break: /^\={3,}$/,
-    line_break: /^ {2}$/,
+
+    PAGE_BREAK: /^\={3,}$/,
+    LINE_BREAK: /^ {2}$/,
 
     emphasis: /(_|\*{1,3}|_\*{1,3}|\*{1,3}_)(.+)(_|\*{1,3}|_\*{1,3}|\*{1,3}_)/g,
     bold_italic_underline: /(_{1}\*{3}(?=.+\*{3}_{1})|\*{3}_{1}(?=.+_{1}\*{3}))(.+?)(\*{3}_{1}|_{1}\*{3})/g,
@@ -29,52 +39,201 @@ const regex = {
     bold_italic: /(\*{3}(?=.+\*{3}))(.+?)(\*{3})/g,
     bold: /(\*{2}(?=.+\*{2}))(.+?)(\*{2})/g,
     italic: /(\*{1}(?=.+\*{1}))(.+?)(\*{1})/g,
-    underline: /(_{1}(?=.+_{1}))(.+?)(_{1})/g,
+    underline: /(_{1}(?=.+_{1}))(.+?)(_{1})/g
 
-    splitter: /\n{2,}/g,
-    cleaner: /^\n+|\n+$/,
-    standardizer: /\r\n|\r/g,
-    whitespacer: /^\t+|^ {3,}/gm
 };
 
-
-class fountainParser  {
-
-    construct(){}
-
-    /**
-     * Cleanup the script and turn the lines into an array
-     * @param  {[type]} script [description]
-     * @return {array}          [description]
-     */
-    lexize(script) {
-        return script.replace(regex.boneyard, '\n$1\n')
-            .replace(regex.standardizer, '\n')
-            .replace(regex.cleaner, '')
-            .replace(regex.whitespacer, '')
-            .split(regex.splitter);
-    };
-
-    /**
-     * [tokenize description]
-     * @param  {array} lines The script lexized
-     * @return [type]         [description]
-     */
-    tokenize(lines) {
-        // const { lexize } = this;
-        // const lines = this.lexize(script);
-        return lines;
-    };
-
-    parse(script) {
-        const { tokenize, lexize } = this;
-
-        const lines = lexize(script);
-        const tokens = tokenize(lines);
-
-        return { parsed: "bees", lines, tokens };
-    };
-};
+export const tokenizeLines = (lines) => {
 
 
-export default new fountainParser();
+    let match, tokens = [];
+
+    for( const i in lines ) {
+        const line = lines[i];
+
+        // // boneyard
+        // if (match = line.match(REGEX.LEXER.BONEYARD)) {
+        //     tokens.push({
+        //         type: match[0][0] === '/' ? 'boneyard_begin' : 'boneyard_end'
+        //     });
+        //     continue;
+        // }
+
+        // centered
+        if (match = line.match(REGEX.CENTERED)) {
+            tokens.push({
+                type: 'centered',
+                text: match[0].replace(/>|</g, '')
+            });
+            continue;
+        }
+
+        // dialogue blocks - characters, parentheticals and dialogue
+        match = line.match(REGEX.DIALOGUE);
+        if (!match) {
+            match = line.match(REGEX.DIALOGUE_POWER_USER);
+        }
+        if (match) {
+
+            // last 2 chars are not spaces
+            if (match[1].indexOf('  ') !== match[1].length - 2) {
+
+                tokens.push({
+                    type: 'dialogue_start',
+                    dual: match[2]=='^'
+                });
+
+                tokens.push({
+                    type: 'character',
+                    text: match[1].trim()
+                });
+
+                const parts = match[3].split(REGEX.PARENTHETICAL_DIALOGUE).reverse();
+
+                // parenthetical found
+                if (parts[1]) {
+                    tokens.push({
+                        type: 'parenthetical',
+                        text: parts[1].trim()
+                    });
+
+                    tokens.push({
+                        type: 'dialogue',
+                        text: parts[0].trim()
+                    });
+
+                } else {
+                    tokens.push({
+                        type: 'dialogue',
+                        text: match[3].trim()
+                    });
+                }
+
+                tokens.push({
+                    type: 'dialogue_end',
+                    dual: match[2]=='^'
+                });
+                continue;
+            }
+        }
+
+        // line breaks
+        if (REGEX.LINE_BREAK.test(line)) {
+            tokens.push({
+                type: 'line_break'
+            });
+            continue;
+        }
+
+        // page breaks
+        if (REGEX.PAGE_BREAK.test(line)) {
+            tokens.push({
+                type: 'page_break'
+            });
+            continue;
+        }
+
+        // notes
+        if (match = line.match(REGEX.NOTE)) {
+            tokens.push({
+                type: 'note',
+                text: match[1]
+            });
+            continue;
+        }
+
+        // scene headings with scene numbers
+        if (match = line.match(REGEX.SCENE_HEADING)) {
+            let text = match[1] || match[2];
+            let scene_number;
+
+            // last 2 chars are not spaces
+            if (text.indexOf('  ') !== text.length - 2) {
+                if (scene_number = text.match(REGEX.SCENE_NUMBER)) {
+                    scene_number = scene_number[2];
+                    text = text.replace(REGEX.SCENE_NUMBER, '');
+                }
+            }
+
+            const token = {
+                type: 'scene_heading',
+                text,
+                scene_number
+            };
+            tokens.push(token);
+            continue;
+        }
+
+        // section
+        if (match = line.match(REGEX.SECTION)) {
+            const token = {
+                type: 'section',
+                text: match[2],
+                level: match[1].length
+            };
+            tokens.push(token);
+            continue;
+        }
+
+        // synopsis
+        if (match = line.match(REGEX.SYNOPSIS)) {
+            const token = {
+                type: 'synopsis',
+                text: match[1]
+            };
+            tokens.push(token);
+            continue;
+        }
+
+        // title page
+        if (REGEX.TITLE_PAGE.test(line)) {
+            match = line.replace(REGEX.TITLE_PAGE, '\n$1').split(REGEX.LEXER.SPLITTER);
+            for (const i in match) {
+                const parts = match[i].replace(REGEX.LEXER.CLEANER, '').split(/\:\n*/);
+                tokens.push({
+                    type: parts[0].trim().toLowerCase().replace(' ', '_'),
+                    text: parts[1].trim()
+                });
+            }
+            continue;
+        }
+
+        // transitions
+        if (match = line.match(REGEX.TRANSITION)) {
+            const token = {
+                type: 'transition',
+                text: match[1] || match[2]
+            };
+            tokens.push(token);
+            continue;
+        }
+
+        // defaut: action
+        tokens.push({
+            type: 'action',
+            text: line
+        });
+
+    }
+
+    return tokens;
+}
+export const lexizeScript = (script) => {
+    return script.replace(REGEX.LEXER.BONEYARD, '\n$1\n')
+        .replace(REGEX.LEXER.STANDARDIZER, '\n')
+        .replace(REGEX.LEXER.CLEANER, '')
+        .replace(REGEX.LEXER.WHITESPACER, '')
+        .split(REGEX.LEXER.SPLITTER);
+}
+
+export const compileTokens = (tokens) => {
+    for( const i in tokens ) {
+        const token = tokens[i];
+        console.log(token);
+    }
+}
+
+export const parseFountainScript = (script) => {
+    const compiled = tokenizeLines( lexizeScript(script) );
+    return { compiled };
+}
