d4aa36d0a7b601af0ae0bba2a17f0f4d69bf8be7
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-03-29 09:27:52 -0400

    starting fountain parser tests

---
+++ src/frontend/js/utility/__tests__/fountain-parser.spec.js
@@ -0,0 +1,12 @@
+import fountainParser from 'utility/fountain-parser';
+
+describe.only('fountainParser', () => {
+    test('parse', () => {
+        const parsed = fountainParser.parse("INT. A PANTS STORT\n\nGOOBER\nI like pie.\n\nBUTTHOLEFACE\nMe toops");
+        console.log(parsed);
+        // expect(fields.username !== undefined);
+        // expect(fields.username == null);
+        // expect(fields.password !== undefined);
+        // expect(fields.username == null);
+    });
+});
