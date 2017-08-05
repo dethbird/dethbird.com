c636549812c8eba0c4d0eaed07e9590d64fb17ec
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-08-04 23:14:55 -0400

    remove semantic and unneedded libs

--- src/frontend/js/pages/index.js
+++ src/frontend/js/pages/index.js
@@ -8,31 +8,7 @@ import App from 'components/app';
 import store from 'store/store';
 
 // External
-import Activate from 'components/pages/activate';
 import Index from 'components/pages/index';
-import Contact from 'components/pages/contact';
-import Newsfeed from 'components/pages/newsfeed';
-import PrivateBeta from 'components/pages/private-beta';
-import PrivateBetaApply from 'components/pages/private-beta-apply';
-import Verify from 'components/pages/verify';
-
-// Admin
-import AdminUsers from 'components/pages/admin-users';
-import AdminUserEdit from 'components/pages/admin-user-edit';
-
-// Internal
-import Dashboard from 'components/pages/dashboard';
-import CharacterEdit from 'components/pages/character-edit';
-import Characters from 'components/pages/characters';
-import ProjectEdit from 'components/pages/project-edit';
-import Project from 'components/pages/project';
-import Projects from 'components/pages/projects';
-import StoryEdit from 'components/pages/story-edit';
-import StoryEditDemo from 'components/pages/story-edit-demo';
-import StoryPlay from 'components/pages/story-play';
-import StoryPlayDemo from 'components/pages/story-play-demo';
-import Stories from 'components/pages/stories';
-import Welcome from 'components/pages/welcome';
 
 // Google Analytics
 ReactGA.initialize('UA-98286537-1', {
@@ -78,37 +54,6 @@ render((
         <Router history={browserHistory} onUpdate={ logPageView }>
             <Route path="/" component={ App } props={ { securityContext } }>
                 <IndexRoute component={ Index } props={ { securityContext } } />
-
-                /** admin */
-                <Route path="admin/users" component={ AdminUsers } props={ { securityContext } } onEnter={ requireAdmin } />
-                <Route path="admin/user/:id/edit" component={ AdminUserEdit } props={ { securityContext } } onEnter={ requireAdmin } />
-
-                /** external */
-                <Route path="activate" component={ Activate } props={ { securityContext, activationUser } } />
-                <Route path="contact" component={ Contact } props={ { securityContext } }/>
-                <Route path="newsfeed" component={ Newsfeed } props={ { securityContext } }/>
-                <Route path="private-beta/apply" component={ PrivateBetaApply } props={ { securityContext } }/>
-                <Route path="private-beta" component={ PrivateBeta } props={ { securityContext } }/>
-                <Route path="product/demo/storyplayer" component={ StoryPlayDemo } props={ { securityContext } }/>
-                <Route path="product/demo/storyeditor" component={ StoryEditDemo } props={ { securityContext } }/>
-                <Route path="verify" component={ Verify } props={ { securityContext, verifyUser } } />
-
-                /** internal */
-                <Route path="projects" component={ Projects } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="project/create" component={ ProjectEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="project/:id/edit" component={ ProjectEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="project/:id" component={ Project } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="dashboard" component={ Dashboard } props={ { securityContext } } onEnter={ requireAuth }/>
-                <Route path="characters" component={ Characters } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="character/create" component={ CharacterEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="character/:id/edit" component={ CharacterEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="stories" component={ Stories } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="story/create" component={ StoryEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="story/create/project/:projectId" component={ StoryEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="story/:id/edit" component={ StoryEdit } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="story/:id/play" component={ StoryPlay } props={ { securityContext } }  onEnter={ requireAuth }/>
-                <Route path="welcome" component={ Welcome } props={ { securityContext } }  onEnter={ requireAuth }/>
-
                 <Route path="*" component={ Index } props={ { securityContext } } />
             </Route>
         </Router>
