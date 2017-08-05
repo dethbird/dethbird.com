0c65ef8b5606d33a257c90a7aa8dfcb86ce28e82
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-08-05 00:19:00 -0400

    index and login routes

--- src/frontend/js/pages/index.js
+++ src/frontend/js/pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import { 
+    BrowserRouter as Router,
+    Route,
+    browserHistory
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReactGA from 'react-ga';
 
-import App from 'components/app';
 import store from 'store/store';
 
 // External
 import Index from 'components/pages/index';
+import Login from 'components/pages/login';
 
 // Google Analytics
 ReactGA.initialize('UA-98286537-1', {
@@ -48,14 +52,15 @@ const logPageView = () => {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
 }
-
+console.log(Index);
+console.log(Login);
 render((
     <Provider store={ store }>
-        <Router history={browserHistory} onUpdate={ logPageView }>
-            <Route path="/" component={ App } props={ { securityContext } }>
-                <IndexRoute component={ Index } props={ { securityContext } } />
-                <Route path="*" component={ Index } props={ { securityContext } } />
-            </Route>
+        <Router history={ browserHistory } onUpdate={ logPageView }>
+            <div>
+                <Route path="/" component={ Index } props={ { securityContext } } />
+                <Route path="/login" component={ Login } props={ { securityContext } } />
+            </div>
         </Router>
     </Provider>
 ), document.getElementById('mount'));
