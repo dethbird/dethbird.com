a46d71d8e34cfcd4c60883d947ca5021e201353e
Rishi Satsangi <rishi.satsangi@gmail.com>
2017-08-10 19:57:40 -0400

    panel comments looks

--- src/frontend/js/components/ui/panel-comments.js
+++ src/frontend/js/components/ui/panel-comments.js
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import TimeAgo from 'react-timeago';
 import ReactMarkdown from 'react-markdown';
 import { List, ListItem } from 'material-ui/List';
+import Paper from 'material-ui/Paper';
 
 
 class PanelComments extends Component {
@@ -13,9 +14,11 @@ class PanelComments extends Component {
         const nodes = panel.comments.map(function(comment,i){
             return (
                 <ListItem key={i} className={classNames(['comment', comment.status])}>
-                    <ReactMarkdown source={ comment.comment || '' } />
-                    <div style={{ textAlign: 'right' }} className="username">{comment.user.username}</div>
-                    <div style={{ textAlign: 'right' }} className="timestamp"><TimeAgo date={comment.date_added} /></div>
+                    <Paper zDepth={1} style={ {padding: 15}}>
+                        <ReactMarkdown source={comment.comment || ''} />
+                        <div style={{ textAlign: 'right' }} className="username">{comment.user.username}</div>
+                        <div style={{ textAlign: 'right' }} className="timestamp"><TimeAgo date={comment.date_added} /></div>
+                    </Paper>
                 </ListItem>
             );
         });
